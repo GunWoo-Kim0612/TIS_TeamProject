6f495ae423a5b07e515e177fa7455a9971aacf47
GunWoo-Kim0612 <gwk0624333@gmail.com>
2022-08-30 02:14:51 +0900

    show table CRUD test완

--- src/main/java/com/tis/myapp/biz/mypick/impl/MyPickImpl.java
+++ src/main/java/com/tis/myapp/biz/mypick/impl/MyPickImpl.java
@@ -1,29 +1,29 @@
-package com.tis.myapp.biz.mypick.impl;
-
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import com.tis.myapp.biz.mypick.MyPickService;
-import com.tis.myapp.biz.mypick.MyPickVO;
-
-@Service("MyPickService")
-public class MyPickImpl implements MyPickService {
-	
-	@Autowired
-	private MyPickDAO mypickDAO;
-	
-
-	@Override
-	public List<MyPickVO> getMypickList(MyPickVO vo) {
-		return mypickDAO.getMypickList(vo);
-	}
-
-	@Override
-	public void deleteMypick(MyPickVO vo) {
-		mypickDAO.deleteMypick(vo);
-	}
-	
-	
-}
+//package com.tis.myapp.biz.mypick.impl;
+//
+//import java.util.List;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+//import com.tis.myapp.biz.mypick.MyPickService;
+//import com.tis.myapp.biz.mypick.MyPickVO;
+//
+//@Service("MyPickService")
+//public class MyPickImpl implements MyPickService {
+//	
+//	@Autowired
+//	private MyPickDAO mypickDAO;
+//	
+//
+//	@Override
+//	public List<MyPickVO> getMypickList(MyPickVO vo) {
+//		return mypickDAO.getMypickList(vo);
+//	}
+//
+//	@Override
+//	public void deleteMypick(MyPickVO vo) {
+//		mypickDAO.deleteMypick(vo);
+//	}
+//	
+//	
+//}
