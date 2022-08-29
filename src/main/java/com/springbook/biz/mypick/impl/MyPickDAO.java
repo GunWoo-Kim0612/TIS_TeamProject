0f101fb4e148b9b4f1849e8e7cfa7f8903b13d81
Administrator <Administrator@121.140.12.246>
2022-08-29 21:23:42 +0900

    220829 added

---
+++ src/main/java/com/springbook/biz/mypick/impl/MyPickDAO.java
@@ -0,0 +1,27 @@
+package com.springbook.biz.mypick.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MyPickDAO {
+	
+	@Autowired
+	private JdbcTemplate jdbcTemplatge;
+	
+}
+
+	private final String MYPICK_DELETE = "";
+	private final String MYPICK_LIST = "";
+	
+	
+	
+	// Delete 
+	public void deleteMypick(MypickVO vo) {
+		System.out.println(">>>S");
+	}
+	
+	
+	
+	// List
+	public 
