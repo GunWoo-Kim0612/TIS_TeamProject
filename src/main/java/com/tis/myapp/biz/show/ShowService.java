85e534a2eb03a574cb8b5222953c23efb8b0bf12
GunWoo-Kim0612 <gwk0624333@gmail.com>
2022-08-29 19:14:49 +0900

    show 기능처리 mybatis

---
+++ src/main/java/com/tis/myapp/biz/show/ShowService.java
@@ -0,0 +1,21 @@
+package com.tis.myapp.biz.show;
+
+import java.util.List;
+
+public interface ShowService {
+	//글등록
+		public void insertShow(ShowVO vo); 
+		
+		//글수정
+		public void updateShow(ShowVO vo);
+		
+		//글삭제
+		public void deleteShow(ShowVO vo); 
+		
+		//글 상세조회
+		public ShowVO getShow(ShowVO vo);
+		
+		
+		//글 전체조회			수정 매개변수 BoardVO vo
+		public List<ShowVO> getShowList(ShowVO vo);
+}
